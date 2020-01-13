f98767185639799339f81a5c41f1ec33db9beb98
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2020-01-13 15:30:34 -0800

    functional buttons

--- src/container/Portfolio.js
+++ src/container/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Portfolio.css';
 import PortfolioItems from '../components/PortfolioItems';
 import portfolioItems from '../../data/portfolioItems';
@@ -6,11 +6,25 @@ import portfolioItems from '../../data/portfolioItems';
 
 export default function Portfolio() {
 
+  const [index, setIndex] = useState(0);
+  
+  const handleIncrement = () => {
+    event.preventDefault();
+    if(index === 3) setIndex(0);
+    else setIndex(index + 1);
+  };
+
+  const handleDecrement = () => {
+    event.preventDefault();
+    if(index === 0) setIndex(3);
+    else setIndex(index - 1);
+  };
+
   return (
     <section className={styles.Portfolio}>
-      <button>decrement</button>
-      <PortfolioItems {...portfolioItems[1]} />
-      <button>increment</button>
+      <button onClick={handleDecrement}>decrement</button>
+      <PortfolioItems {...portfolioItems[index]} />
+      <button onClick={handleIncrement}>increment</button>
     </section>
   );
 }
